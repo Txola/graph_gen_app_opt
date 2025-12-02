1122bea71185bb733b70c06f1039a43c6d114a0a
Beñat Solaegui <solaeguibenat@gmail.com>
2025-12-02 10:20:40 +0100

    Add script to sample a qm9 graph #1

---
+++ src/sample.py
@@ -0,0 +1,50 @@
+import time
+
+import hydra
+from flow_matching.sampler import QM9CondSampler
+from metrics.molecular_metrics import compute_validity
+from metrics.qm9_info import QM9Infos
+from models.extra_features import ExtraFeatures
+from models.extra_features import ExtraMolecularFeatures
+from omegaconf import DictConfig
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="config")
+def main(cfg: DictConfig):
+    print(cfg)
+
+    qm9_infos = QM9Infos()
+
+    extra_features = ExtraFeatures(
+        cfg.model.extra_features,
+        cfg.model.rrwp_steps,
+        dataset_info=qm9_infos,
+    )
+    domain_features = ExtraMolecularFeatures(dataset_infos=qm9_infos)
+    sampler = QM9CondSampler(
+        cfg,
+        qm9_dataset_infos=qm9_infos,
+        extra_features=extra_features,
+        domain_features=domain_features,
+        eta=0,
+        omega=1,
+        distortion="polydec",
+    )
+    print("Sampling...")
+    start = time.time()
+    samples, _ = sampler.sample(
+        batch_size=1,
+        sample_steps=100,
+        condition_value=-400,
+    )
+    end = time.time()
+    print(f"Sampling took {end - start:.4f} seconds.")
+
+    start = time.time()
+    print(compute_validity(samples))
+    end = time.time()
+    print(f"Validity computation took {end - start:.4f} seconds.")
+
+
+if __name__ == "__main__":
+    main()
